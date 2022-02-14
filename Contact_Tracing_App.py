c1289f07739fb08cca10a0ecfd67be9efbd2f73a
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2022-02-14 15:10:08 +0800

    Info from the qrcode is being displayed on top of the qrcode while detecting it using the camera. Data retrieved was also being transferred on a text file named contact_tracing.txt.

--- Contact_Tracing_App.py
+++ Contact_Tracing_App.py
@@ -8,7 +8,10 @@ def decode_qrcode(frame):
         qrcode_data = code.data.decode('utf-8')
         cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
 
-        print(qrcode_data + "\n------------------")
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, qrcode_data, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
+        with open("contact_tracing.txt", mode ='w') as file:
+            file.write("DATA RETRIEVED:\n\n" + qrcode_data)
     return frame
 
 
